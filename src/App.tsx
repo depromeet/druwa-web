a312bb18bbaf533c6eab67ce69eafeb92991a71d
seokju-na <seokju.me@gmail.com>
2019-12-22 16:33:16 +0900

    Add react routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import IntroPage from './pages/IntroPage';
+
+const HomePage = lazy(() => import('./pages/HomePage'));
 
 export default function App() {
-  return <div>Hello World!</div>;
+  return (
+    <Router>
+      <Link to="/home">Home</Link>
+      <Suspense fallback={null}>
+        <Switch>
+          <Route exact={true} path="/" component={IntroPage} />
+          <Route path="/home" component={HomePage} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
 }
