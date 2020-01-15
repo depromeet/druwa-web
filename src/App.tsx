391c55fb3000f7b0440fe8c57d64058386859e45
seokju-na <seokju.me@gmail.com>
2020-01-16 00:22:11 +0900

    메인에 버튼 표시

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { UIProvider } from './core';
+import { defaultDarkTheme } from './styles';
+import { Button } from './ui';
 
 export default function App() {
-  return <div>Hello World!</div>;
+  return (
+    <UIProvider theme={defaultDarkTheme}>
+      <Button>Normal</Button>
+      <Button color="primary">Primary</Button>
+      <Button color="accent">Accent</Button>
+      <Button color="dark">Dark</Button>
+    </UIProvider>
+  );
 }
