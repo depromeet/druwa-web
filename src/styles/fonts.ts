366556b2eef26c2179057b2c5eec59344f43a78b
seokju-na <seokju.me@gmail.com>
2020-01-16 00:21:13 +0900

    스타일 정보 추가

---
+++ src/styles/fonts.ts
@@ -0,0 +1,33 @@
+import { css } from '@emotion/core';
+
+export enum FontFamily {
+  SansSerif = 'sans-serif',
+}
+
+export const cssSansSerif = css`
+  font-family: Roboto, 'Noto Sans KR', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Apple SD Gothic Neo, 'Helvetica Neue', Arial, sans-serif;
+`;
+
+export type FontSize = 'small' | 'regular' | 'medium' | 'big';
+export type FontWeight = 'regular' | 'medium' | 'bold' | 'black';
+export type LinHeight = 'normal' | 'condensed';
+
+export const fontSizes: Readonly<Record<FontSize, number>> = {
+  small: 11,
+  regular: 14,
+  medium: 16,
+  big: 20,
+};
+
+export const fontWeights: Readonly<Record<FontWeight, number>> = {
+  regular: 400,
+  medium: 500,
+  bold: 700,
+  black: 900,
+};
+
+export const lineHeights: Readonly<Record<LinHeight, number>> = {
+  normal: 1.43,
+  condensed: 1.17,
+};
---
+++ src/styles/index.ts
@@ -0,0 +1,4 @@
+export * from './fonts';
+export * from './mixins';
+export * from './palette';
+export * from './theming';
