c588e4dac999372f2309ea11c7d8d7c5b4cf3f4d
seokju-na <seokju.me@gmail.com>
2020-01-18 16:46:36 +0900

    OAuth API Url 추가

--- src/remotes/api.ts
+++ src/remotes/api.ts
@@ -1,9 +1,16 @@
+import { publicUrl } from '../environment';
 import { User } from '../models';
 import { rxHttp } from './rx-http';
 import { WithToken } from './types';
 
 const API_URL = 'https://api.druwa.site';
 const apiUrl = (path: string) => `${API_URL}${path}`;
+
+const oauthRedirectUrl = encodeURIComponent(`${publicUrl}/oauth/check`);
+
+export const oauthApiUrls = {
+  kakao: apiUrl(`/oauth2/authorize/kakao?redirect_uri=${oauthRedirectUrl}`),
+} as const;
 
 const authorizationHeader = (token: string) => ({
   Authorization: `JWT ${token}`,
