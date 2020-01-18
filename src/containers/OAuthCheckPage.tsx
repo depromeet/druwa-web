86bbdc2a9c5efa9590de04d2818788e6066b84a7
seokju-na <seokju.me@gmail.com>
2020-01-18 17:24:37 +0900

    OAuth 인증 후에 앱으로 토큰 전달하는 로직 추가

--- src/containers/OAuthCheckPage.tsx
+++ src/containers/OAuthCheckPage.tsx
@@ -1,13 +1,22 @@
 import React, { useEffect } from 'react';
 import QS from 'query-string';
 import { kakaoOAuthPopupName } from '../constants';
+import { appBridge } from '../core';
 import { closeThisPopup, postMessageToOpener } from '../hooks';
 
 export default function OAuthCheckPage() {
   useEffect(() => {
     const parsed: { token?: string } = QS.parse(location.search);
+    const token = parsed?.token;
 
-    postMessageToOpener(kakaoOAuthPopupName, parsed?.token);
+    // 앱으로 인증 토큰을 전달합니다.
+    appBridge.postMessage({
+      name: 'oauthToken',
+      token: token ?? null,
+    });
+
+    // 부모 웹페이지에 인증 토큰을 전달합니다.
+    postMessageToOpener(kakaoOAuthPopupName, token);
     closeThisPopup(kakaoOAuthPopupName);
   }, []);
 
