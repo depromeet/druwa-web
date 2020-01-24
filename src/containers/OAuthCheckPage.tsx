5caaca686fd9d2539978b83eada3ea8262a62bde
seokju-na <seokju.me@gmail.com>
2020-01-24 19:14:01 +0900

    토큰값이 없을 시 에외처리

--- src/containers/OAuthCheckPage.tsx
+++ src/containers/OAuthCheckPage.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
 import QS from 'query-string';
+import React, { useEffect } from 'react';
 import { kakaoOAuthPopupName } from '../constants';
 import { appBridge } from '../core';
 import { closeThisPopup, postMessageToOpener } from '../hooks';
@@ -8,6 +8,12 @@ export default function OAuthCheckPage() {
   useEffect(() => {
     const parsed: { token?: string } = QS.parse(location.search);
     const token = parsed?.token;
+
+    if (token === undefined) {
+      alert('카카오톡 로그인에 실패하였습니다.');
+      closeThisPopup(kakaoOAuthPopupName);
+      return;
+    }
 
     // 앱으로 인증 토큰을 전달합니다.
     appBridge.postMessage({
