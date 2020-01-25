9957e4430d8d304f6850af6fcfb4642c852ed038
seokju-na <seokju.me@gmail.com>
2020-01-25 16:52:18 +0900

    IntersectionObserver 훅 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.scss';
 import AppShell from './containers/AppShell';
+import './polyfills';
 
 const root = document.getElementById('root');
 
